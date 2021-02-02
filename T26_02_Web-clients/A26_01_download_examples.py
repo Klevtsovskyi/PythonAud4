d2daddb11d9fcbd33699a67be1d48a7013480b3d
Arsen <avklevtsovskiy@gmail.com>
2021-02-02 13:31:13 +0200

    update T26

---
+++ T26_02_Web-clients/A26_01_download_examples.py
@@ -0,0 +1,143 @@
+""" A26.01
+Скласти програму, яка читає з сайту кафедри математичної фізики
+(http://matfiz.univ.kiev.ua) та завантажує у заданий каталог усі
+приклади програм із заданої теми.
+Сторінка з адресами тем - http://matfiz.univ.kiev.ua/pages/13
+Посилання на сторінку теми на сторінці з адресами має вигляд:
+<a href=[url-шлях]/pages/YY" >Тема XX
+де [url-шлях] – відносний шлях до сторінки (по відношенню до
+http://matfiz.univ.kiev.ua) YY – номер сторінки, XX – номер теми
+
+Посилання на приклади на сторінці теми має вигляд:
+href="/userfiles/files/[файл]"
+де [файл] – ім’я файлу прикладу.
+Файли прикладів можуть мати розширення .py або .pyw
+
+Використати структурний аналіз HTML за допомогою класу HTMLParser
+з модуля html.parser.
+"""
+
+
+from urllib.request import urlopen      # Функція для отримання веб-сторінки з мережі
+from urllib.request import urlretrieve  # Функція для завантаження файлу з мережі
+import re
+import os
+from html.parser import HTMLParser
+
+
+# Шаблон для отрримання назви файлу з його URL-адреси
+FILENAME = r'.+/(?P<NAME>.+)'
+
+
+class TopicsViewParser(HTMLParser):
+    """ Клас для знаходження посилання на тему з номером n.
+    """
+
+    def __init__(self, n):
+        super().__init__()
+        self.topic = "Тема {}.".format(n)  # Номер теми
+        self.in_a = False  # Чи знаходимось в тегу <a>
+        self.attrs = None  # Атрибути тегу
+        self.url = None    # Посилання на веб-сторінку теми № n
+
+    def handle_starttag(self, tag, attrs):
+        if not self.url:
+            # Якщо заходимо в тег <a> з класом "list-group-item"
+            # (class="list-group-item")
+            if tag == "a" and ("class", "list-group-item") in attrs:
+                self.in_a = True
+                self.attrs = attrs
+
+    def handle_data(self, data):
+        if not self.url:
+            # Якщо знаходимося в тегу <a> та
+            # його дані починаються з рядка "Тема {}."
+            if self.in_a and data.strip().startswith(self.topic):
+                # Отримуємо атрибут з посиланням (href="...")
+                self.url = dict(self.attrs)["href"]
+
+    def handle_endtag(self, tag):
+        if not self.url:
+            # Якщо виходимо з тегу <a>
+            if tag == "a":
+                self.in_a = False
+
+
+class PyFilesViewParser(HTMLParser):
+    """ Клас для знаходження всіх python-файлів з html-документу.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.pyfiles = []  # Список знайдених python-файлів
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            href = dict(attrs)["href"]
+            # Якщо посилання закінчується на .py чи .pyw, то
+            # додаємо його до списку
+            if href.endswith(".py") or href.endswith(".pyw"):
+                self.pyfiles.append(href)
+
+
+def download_examples(n, dirname=""):
+    """ З сайту http://matfiz.univ.kiev.ua завантажує
+    усі python-файли за номером теми n в папку dirname,
+    якщо dirname не вказано, то завантажує файли у поточну папку.
+    """
+    main_url = "http://matfiz.univ.kiev.ua"
+    # Отримуємо дані веб-сторінки з темами
+    topics_html = get_html(main_url + "/pages/13")
+    # Шукаємо посилання на веб-сторінку з n-тою темою
+    # та отримуємо її повну адресу
+    tvp = TopicsViewParser(n)
+    tvp.feed(topics_html)
+    topic_url = main_url + tvp.url
+    tvp.close()
+    # Отримуємо сторінку n-тою темою
+    topic_html = get_html(topic_url)
+    # Створюємо каталог для збереження файлів
+    if dirname and not os.path.exists(dirname):
+        os.mkdir(dirname)
+    # Знаходимо список відносних посилань на python-файли
+    pfvp = PyFilesViewParser()
+    pfvp.feed(topic_html)
+    pyfiles = pfvp.pyfiles
+    pfvp.close()
+    # Ітеруємо по всім посиланням на python-файли
+    for example in pyfiles:
+        example_url = main_url + example # Повне посилання на файл
+        filename = get_filename(example) # Визначаємо ім'я файлу
+        # Завантажуємо файл і зберігаємо їх у папці dirname
+        urlretrieve(example_url, os.path.join(dirname, filename))
+
+
+def get_html(url):
+    """ Повертає розкодавані дані веб-сторінки за заданою адресою."""
+    return str(urlopen(url).read(), encoding="utf-8", errors="ignore")
+
+
+def find_topic_url(html, n):
+    """ Шукає і повертає посилання на тему з номером n
+    у заданому рядку.
+
+    :param html: рядок html-типу
+    :param n: номер шуканої теми
+    """
+    return re.search(A_TAG_TOPIC.format(n), html).group("URL")
+
+
+def find_examples_urls(html):
+    """ Повертає список усіх відносних посилань у заданому
+    рядку на файли з розширенням .py або .pyw
+    """
+    return re.findall(PYFILE, html)
+
+
+def get_filename(example):
+    """ Повертає у заданій віносній URL-адресі ім'я python-файлу"""
+    return re.search(FILENAME, example).group("NAME")
+
+
+if __name__ == '__main__':
+    download_examples(30, "examples")
---
+++ T26_02_Web-clients/A26_02_station_code.py
@@ -0,0 +1,128 @@
+""" A26.02 (T26.15*)
+Скласти програму, яка отримує <Код Експрес-3> відповідної
+станції зі сторінки Вікіпедії за запитом
+https://uk.wikipedia.org/wiki/Список_залізничних_станцій_і_роз'їздів_України_(<перша літера назви міста>)
+Наприклад, для станції Київ-Пасажирський відповідний запит на отримання сторінки з кодами -
+https://uk.wikipedia.org/wiki/Список_залізничних_станцій_і_роз'їздів_України_(К)
+В такому випадку код буде мати такий вигляд:
+2200001
+
+Використати структурний аналіз HTML за допомогою класу HTMLParser
+з модуля html.parser.
+"""
+
+from urllib.request import urlopen  # Функція для отримання веб-сторінки з мережі
+from urllib.parse import quote      # Функція для кодування кирилиці в url
+from urllib.error import HTTPError  # Виключення, яке може виникати при відповіді веб-сервера
+from html.parser import HTMLParser
+
+
+class StationViewParser(HTMLParser):
+    """ Клас, який дозволяє виконати аналіз html-файлу та знайти
+    код експрес 3 за заданою залізничною станцією.
+
+    Щоб запустити пошук, потрібно викликати
+    метод StationViewParser.feed(self, data),
+    де data - дані з html-файлу.
+    """
+
+    def __init__(self, station):
+        super().__init__()
+        self.station = station    # Назва станції
+        self.station_code = None  # Шуканий код станції
+        self.entered_tr = False   # Чи увійшли в тег <tr>
+        self.in_first_a = False   # Чи знаходимося в першому тегу <a>?
+        self.found_a = False      # Чи знайшли тег <a> з шуканю назвою станції?
+        # Лічильник тегів <td>, які потрібно відрахувати щоб знайти потрібний
+        self.td_count = 0
+        # Чи знайшли потрібний тег <td>, в якому знаходить код станції?
+        self.in_code_td = False
+
+    def handle_starttag(self, tag, attrs):
+        """ Метод, який викликається,
+        коли зустрічаємо відкриття тегу (<tag>).
+
+        :param tag: назва тегу (напр., для <a> маємо tag == 'a')
+        :param attrs: список кортежів ключ-значення атрибутів тегу
+                      (напр., для <a href="/wiki/" title="name">
+                       маємо attrs == [('href', '/wiki/'), ('title', 'name')]).
+        """
+        if not self.station_code:
+            # Якщо входимо в тег <tr>
+            if tag == "tr":
+                self.entered_tr = True
+            # Якщо зустрічаємо перший тег <a> після <tr>
+            elif self.entered_tr and tag == "a":
+                self.in_first_a = True
+            # Якщо знайшли потрібний тег <a>, починаєто відраховувати теги <td>
+            elif self.found_a and tag == 'td':
+                # Якщо відрахували потрібну кількість тегів <td>,
+                if self.td_count == 4:
+                    self.in_code_td = True  # то знаходимося в порібному тегу
+                else:
+                    self.td_count += 1
+
+    def handle_endtag(self, tag):
+        """ Метод, який викликається,
+        коли зустрічаємо закриття тегу (</tag>).
+
+        :param tag: назва тегу (напр., для </a> маємо tag == 'a').
+        """
+        if not self.station_code:
+            if tag == "a" and self.in_first_a:  # Якщо виходимо з тегу <a>
+                self.in_first_a = False
+                self.entered_tr = False
+
+    def handle_data(self, data):
+        """ Метод, який викликається,
+        коли зустрічаємо дані (тобто все за винятком тегів).
+
+        :param data: рядок даних
+        """
+        if not self.station_code:
+            # Якщо дані в першому тегу <a> співпадають з потрібною станцією
+            if self.in_first_a and data == self.station:
+                self.found_a = True  # Шуканий тег <a> знайдено
+            elif self.in_code_td:    # Якщо знаходимося в потрібному тегу <td>,
+                self.station_code = data  # присвоюємо дані і завершуємо пошук
+
+
+def get_station_code(station):
+    """
+    Повертає Код Експрес-3 заданої станції.
+
+    :param station: назва станції
+    :return: код станції
+    """
+    # Будуємо URL-запит
+    url = "https://uk.wikipedia.org/wiki/"
+    path = ("Список_залізничних_станцій_і_роз'їздів_України_({})"
+            .format(station[0]))
+    # Кодуємо кирилицю в URL-запиті та визначаємо остаточний URL-запит
+    full_url = url + quote(path, encoding="utf-8")
+    try:
+        # Отримуємо веб-сторінку з мережі
+        request = urlopen(full_url)
+        # Розкодовуємо веб-сторінку
+        html = str(request.read(), encoding="utf-8", errors="ignore")
+        # Створюємо об'єкт для структурного аналізу html
+        svp = StationViewParser(station)
+        svp.feed(html)           # Передаємо дані
+        code = svp.station_code  # Отримаємо результат
+        svp.close()              # Звільняємо буфер
+        return code
+    # У випадку помилки (неуспішного статусу відповіді), друкуємо її значення
+    except HTTPError as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    stations = ["Київ-Пасажирський",
+                "Львів",
+                "Київ"]
+    for station in stations:
+        code = get_station_code(station)
+        if code:
+            print(station, ":", code)
+        else:
+            print("Для", station, "кода не знайдено")
